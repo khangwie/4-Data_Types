d620b7b5c59380a3ad83ed9b43c2dd3ac8a0ef5c
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-09 11:35:43 +0200

    Add project files.

---
+++ 4 Data_Types/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaration
+            string name ="John";
+            int age =35;
+            bool isTall =false;
+            double weight =80.2;
+
+            /*for the smallest least precise decimal use float,
+             * for more precise use decimal eg for money,
+             * in most cases double will do just fine
+             * */
+            Console.WriteLine(name + "is" +  age + "he weighs " + weight);
+            Console.WriteLine("it is"  + isTall + "that he is tall");
+
+            //freeze console
+            Console.ReadLine();
+
+
+                
+        }
+    }
+}
